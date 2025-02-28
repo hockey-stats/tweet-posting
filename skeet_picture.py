e038aaa4ee919b82592df8e23c97882a9d7b5407
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-02-28 12:31:26 -0500

    fixed aspect ratios for images in skeets

--- skeet_picture.py
+++ skeet_picture.py
@@ -9,6 +9,8 @@ Login info for BlueSky saved as repository variables in GitHub
 import os
 import argparse
 from atproto import Client, client_utils
+from atproto_client.models.app.bsky.embed.defs import AspectRatio
+
 
 # Set env variables from repo secrets
 BSKY_LOGIN = os.environ['BSKY_LOGIN']
@@ -44,29 +46,43 @@ def parse_text(text):
     return text_builder, plain_text
 
 
-def send_skeet(image, post_body, alt_text):
+def send_skeet(image, post_body, alt_text, aspect_ratio):
     """
     Given an image and text, login to the BlueSky client and send a skeet containing them.
     :param str image: Filepath to image which will be skeeted.
     :param str post_body: Text body of the skeet.
     :param str alt_text: Alt text for the skeet image.
+    :param str apect_ratio: Aspect ratio to use for image in the form '{width},{height}'
     """
     client = Client()
 
     client.login(BSKY_LOGIN, BSKY_PASSWORD)
 
+    # Aspect Ratio needs to be passed in as an AspectRatio object, parse the provided string
+    # for width and height and construct the object. Raise an error if the provided string
+    # isn't in proper format.
+    try:
+        width, height = aspect_ratio.split(',')
+        image_aspect_ratio = AspectRatio(width=int(width), height=int(height))
+    except ValueError as e:
+        print("Aspect ratio provided in incorrect format!\n"\
+              "Expected: '{width},{height}', e.g. '10,8'\n"\
+              f"Recieved: '{aspect_ratio}'")
+        raise e
+
     with open(image, 'rb') as f:
         img_data = f.read()
 
-        client.send_image(text=post_body, image=img_data, image_alt=alt_text)
+        client.send_image(text=post_body, image=img_data, image_alt=alt_text, 
+                          image_aspect_ratio=image_aspect_ratio)
 
 
-def main(image, text):
+def main(image, text, aspect_ratio):
     """
     Main function
     """
     post_body, alt_text = parse_text(text)
-    send_skeet(image, post_body, alt_text)
+    send_skeet(image, post_body, alt_text, aspect_ratio)
 
 
 if __name__ == '__main__':
@@ -75,6 +91,9 @@ if __name__ == '__main__':
                         help='Path to image file which is to be skeeted.')
     parser.add_argument('-t', '--text', default='',
                         help='Text to be skeeted alongside image, defaults to empty string.')
+    parser.add_argument('-a', '--aspect_ratio', default='10,8',
+                        help='Aspect ratio to be used for image post. E.g., standard plots usually'\
+                             'use 10x8, but game reports use 20x14. Defaults to "10,8".')
     args = parser.parse_args()
 
-    main(image=args.image, text=args.text)
+    main(image=args.image, text=args.text, aspect_ratio=args.aspect_ratio)
